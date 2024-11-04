9131548cd6b7f46cd0db88d2460ac303a208b9f5
Mihir72999 <mihir72999@gmail.com>
2024-11-04 11:34:20 +0530

    Update is IsError Page

--- src/component/IsError.jsx
+++ src/component/IsError.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const IsError = () => {
   return (
     <div className='flex flex-col items-center justify-center max-h-dvh'>
-       <h1 className='text-2xl mb-3'>Something Went Wrong Please Try Again</h1>
+       <h1 className='text-2xl tracking-tighter mb-3 font-semibold leading-tight'>Something Went Wrong Please Try Again</h1>
        <button onClick={()=>window.location.reload()} className='flex justify-center items-center px-4 py-2 rounded-lg
-        bg-red-950 text-white'>Try Again</button> 
+        bg-red-950 text-white font-bold'>Try Again</button> 
     </div>
   )
 }
