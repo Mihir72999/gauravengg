ec72eee74f70ad41a30827e126930b8acc438cb6
Mihir72999 <mihir72999@gmail.com>
2024-11-04 10:02:40 +0530

    Updated is  Error Page

--- src/component/IsLoading.jsx
+++ src/component/IsLoading.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const IsLoading = () => {
   return (
-    <div>
-     <p>Welcome to the Gaurav Engineering <br/> Document is Loading Please Wait<span className=' text-2xl'>...</span></p>  
+    <div className='h-auto relative'>
+     <p>Welcome to the Gaurav Engineering <br/> Document is Loading Please Wait</p>  
+     <div  className=' absolute right-0 bottom-1 h-1 w-1 text-2xl animate-ping bg-green-700 rounded-full'/>  
+     <div  className=' absolute right-2 bottom-1 h-1 w-1 text-2xl animate-ping bg-green-700 rounded-full'/>  
+     <div  className=' absolute right-4 bottom-1 h-1 w-1 text-2xl animate-ping bg-green-700 rounded-full'/>
     </div>
+    
   )
 }
 
