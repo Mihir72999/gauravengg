ecf4a5bcc7402f8e5cc8b08e3ad67ef615014611
Mihir72999 <mihir72999@gmail.com>
2024-11-04 11:21:03 +0530

    change loading dots

--- src/component/IsLoading.jsx
+++ src/component/IsLoading.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const IsLoading = () => {
   return (
     <div className='h-auto relative'>
-     <p>Welcome to the Gaurav Engineering <br/> Document is Loading Please Wait</p>  
-     <div  className=' absolute right-0 bottom-1 h-1 w-1 text-2xl animate-ping bg-green-700 rounded-full'/>  
+     <p className='mr-1'>Welcome to the Gaurav Engineering <br/> Document is Loading Please Wait</p>  
+    
+     <div  className='absolute right-0 bottom-1 h-1 w-1 text-2xl animate-ping bg-green-700 rounded-full'/>  
      <div  className=' absolute right-2 bottom-1 h-1 w-1 text-2xl animate-ping bg-green-700 rounded-full'/>  
      <div  className=' absolute right-4 bottom-1 h-1 w-1 text-2xl animate-ping bg-green-700 rounded-full'/>
     </div>
